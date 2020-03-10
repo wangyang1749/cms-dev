b5b3c27303b2863b2016caa364c58124553fcc11
wangyang1749 <1749748955@qq.com>
2020-03-11 02:36:01 +0800

    update

--- cms-web/src/main/java/com/wangyang/cms/config/CustomUrlDecisionManager.java
+++ cms-web/src/main/java/com/wangyang/cms/config/CustomUrlDecisionManager.java
@@ -1,6 +1,7 @@
 package com.wangyang.cms.config;
 
 import com.wangyang.authorize.exception.UserException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
@@ -16,18 +17,20 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
 
 @Component
+@Slf4j
 public class CustomUrlDecisionManager implements AccessDecisionManager {
     @Override
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
-        HttpServletRequest request = ((FilterInvocation) object).getRequest();
-        if(!StringUtils.isEmpty(request.getParameter("access_token"))){
-            if(request.getParameter("access_token").equals("123456")){
-                return;
-            }
-        }
+//        HttpServletRequest request = ((FilterInvocation) object).getRequest();
+//        if(!StringUtils.isEmpty(request.getParameter("access_token"))){
+//            if(request.getParameter("access_token").equals("123456")){
+//                return;
+//            }
+//        }
 
         for (ConfigAttribute configAttribute : configAttributes) {
             String needRole = configAttribute.getAttribute();
+            log.info("该路径需要的权限"+needRole);
             if(needRole.equals("ROLE_LOGIN")){
                 if (authentication instanceof AnonymousAuthenticationToken) {
                     throw new AccessDeniedException("尚未登录，请登录!");
@@ -38,11 +41,13 @@ public class CustomUrlDecisionManager implements AccessDecisionManager {
             //获取用户权限, 与当前权限进行比较
             Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
             for (GrantedAuthority authority : authorities) {
+                log.info("当前用户具有的权限"+authority.getAuthority());
                 if (authority.getAuthority().equals(needRole)) {
                     return;
                 }
             }
         }
+        log.info("权限不足，请联系管理员!!");
 //        throw new AccessDeniedException("权限不足，请联系管理员!");
         throw new AccessDeniedException("权限不足，请联系管理员!!");
     }
