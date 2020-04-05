460f94c89f41ac2ff065ff70020f949731d3fb49
wangyang1749 <1749748955@qq.com>
2020-04-05 22:09:24 +0800

    update

--- cms-web/src/test/java/com/wangyang/cms/service/TestCommentService.java
+++ cms-web/src/test/java/com/wangyang/cms/service/TestCommentService.java
@@ -3,6 +3,9 @@ package com.wangyang.cms.service;
 import com.wangyang.cms.pojo.entity.Comment;
 import org.junit.jupiter.api.Test;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public class TestCommentService extends AbstractServiceTest {
 
     @Test
